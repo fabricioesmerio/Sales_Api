0b167118c2d376ceab6bd1f924c05beeb2f910e9
fabricioesmerio <fabricio1esmerio@gmail.com>
2025-10-23 23:46:45 -0300

    feat: criado SaleDto e SaleItemDto

---
+++ src/Sales.Application/Dto/SaleDto.cs
@@ -0,0 +1,15 @@
+﻿
+
+namespace Sales.Application.Dto
+{
+    public class SaleDto
+    {
+        public string SaleNumber { get; set; } = default!;
+        public DateTime Date { get; set; }
+        public string Customer { get; set; } = default!;
+        public string Branch { get; set; } = default!;
+        public bool IsCancelled { get; set; }
+        public List<SaleItemDto> Items { get; set; } = new();
+        public decimal TotalAmount { get; set; }
+    }
+}
---
+++ src/Sales.Application/Dto/SaleItemDto.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace Sales.Application.Dto
+{
+    public class SaleItemDto
+    {
+        public string Product { get; set; } = default!;
+        public int Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+        public decimal Discount { get; set; }
+        public decimal Total { get; set; }
+    }
+}

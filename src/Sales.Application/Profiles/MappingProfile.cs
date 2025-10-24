a3655d90bbbda4fa11e849ed228db9b48b80a831
fabricioesmerio <fabricio1esmerio@gmail.com>
2025-10-23 23:53:38 -0300

    Adicionado MappingProfile

---
+++ src/Sales.Application/Profiles/MappingProfile.cs
@@ -0,0 +1,17 @@
+﻿
+
+using AutoMapper;
+using Sales.Application.Dto;
+using Sales.Domain.Entities;
+
+namespace Sales.Application.Profiles
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Sale, SaleDto>().ReverseMap();
+            CreateMap<SaleItem, SaleItemDto>().ReverseMap();
+        }
+    }
+}

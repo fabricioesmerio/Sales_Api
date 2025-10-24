94b7c05c4b17aa0d658dbd239ed42dc9344564b0
fabricioesmerio <fabricio1esmerio@gmail.com>
2025-10-23 23:58:59 -0300

    Adiciona SaleRepository

---
+++ src/Sales.Application/Interfaces/ISaleRepository.cs
@@ -0,0 +1,16 @@
+﻿
+
+using Sales.Domain.Entities;
+
+namespace Sales.Application.Interfaces
+{
+    public interface ISaleRepository
+    {
+        Task<Sale?> GetByIdAsync(Guid id);
+        Task AddAsync(Sale sale);
+        Task UpdateAsync(Sale sale);
+        Task DeleteAsync(Guid id);
+        Task<IEnumerable<Sale>> GetAllAsync();
+        Task SaveChangesAsync();
+    }
+}
---
+++ src/Sales.Infrastructure/Repositories/SaleRepository.cs
@@ -0,0 +1,46 @@
+﻿
+
+using Microsoft.EntityFrameworkCore;
+using Sales.Application.Interfaces;
+using Sales.Domain.Entities;
+using Sales.Infrastructure.Persistence;
+
+namespace Sales.Infrastructure.Repositories
+{
+    public class SaleRepository : ISaleRepository
+    {
+        private readonly AppDbContext _context;
+        
+        public SaleRepository(AppDbContext context) => _context = context;
+
+        public async Task AddAsync(Sale sale)
+        {
+            await _context.Sales.AddAsync(sale);
+        }
+
+        public async Task DeleteAsync(Guid id)
+        {
+            var entity = await _context.Sales.FindAsync(id);
+
+            if (entity is not null) 
+                _context.Sales.Remove(entity);
+        }
+
+        public async Task<IEnumerable<Sale>> GetAllAsync()
+        {
+            return await _context.Sales.Include(s => s.Items).ToListAsync();
+        }
+
+        public async Task<Sale?> GetByIdAsync(Guid id)
+        {
+            return await _context.Sales.Include(s => s.Items).FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        public async Task SaveChangesAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(Sale sale) => _context.Sales.Update(sale);
+    }
+}

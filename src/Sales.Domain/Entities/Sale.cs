ebd0715a58d603b1fdbc1ebf15d3e6a08f884f09
fabricioesmerio <fabricio1esmerio@gmail.com>
2025-10-23 23:42:10 -0300

    Criado entidade de Sale e SaleItem

---
+++ src/Sales.Domain/Entities/Sale.cs
@@ -0,0 +1,35 @@
+﻿
+
+namespace Sales.Domain.Entities
+{
+    public class Sale
+    {
+        public Guid Id { get; set; }
+        public string SaleNumber { get; set; } = default!;
+        public DateTime Date { get; set; }
+        public string Customer { get; set; } = default!;
+        public string Branch { get; set; } = default!;
+        public bool IsCancelled { get; set; }
+
+        public decimal TotalAmount => Items.Sum(i => i.Total);
+        public List<SaleItem> Items { get; set; } = new();
+
+        public void AddItem(SaleItem item)
+        {
+            // Regras de negócio de quantidade e desconto
+            if (item.Quantity > 20)
+                throw new InvalidOperationException("Cannot sell more than 20 identical items.");
+
+            if (item.Quantity >= 10)
+                item.Discount = 0.20m;
+            else if (item.Quantity >= 4)
+                item.Discount = 0.10m;
+            else
+                item.Discount = 0.00m;
+
+            Items.Add(item);
+        }
+
+        public void Cancel() => IsCancelled = true;
+    }
+}
---
+++ src/Sales.Domain/Entities/SaleItem.cs
@@ -0,0 +1,14 @@
+﻿
+
+namespace Sales.Domain.Entities
+{
+    public class SaleItem
+    {
+        public Guid Id { get; set; }
+        public string Product { get; set; } = default!;
+        public int Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+        public decimal Discount { get; set; }
+        public decimal Total => Quantity * UnitPrice * (1 - Discount);
+    }
+}

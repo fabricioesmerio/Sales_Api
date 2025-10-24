6c63924de512ba516ea40af69df39e6ef4aa0218
fabricioesmerio <fabricio1esmerio@gmail.com>
2025-10-24 00:01:03 -0300

    Adiciona SalesController

---
+++ src/Sales.Api/Controllers/SalesController.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Sales.Application.Commands;
+using Sales.Application.Dto;
+
+namespace Sales.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class SalesController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public SalesController(
+            IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(SaleDto dto)
+        => Ok(await _mediator.Send(new CreateSaleCommand(dto)));
+
+        
+    }
+}

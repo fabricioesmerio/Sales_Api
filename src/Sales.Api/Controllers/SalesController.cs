25141eba1d57ceee97cbe12c4e64468d36059255
fabricioesmerio <fabricio1esmerio@gmail.com>
2025-10-24 00:08:19 -0300

    Adiciona busca de todas as vendas

--- src/Sales.Api/Controllers/SalesController.cs
+++ src/Sales.Api/Controllers/SalesController.cs
@@ -1,7 +1,9 @@
-﻿using MediatR;
+﻿using AutoMapper;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Sales.Application.Commands;
 using Sales.Application.Dto;
+using Sales.Application.Interfaces;
 
 namespace Sales.Api.Controllers
 {
@@ -10,17 +12,26 @@ namespace Sales.Api.Controllers
     public class SalesController : ControllerBase
     {
         private readonly IMediator _mediator;
+        private readonly ISaleRepository _repo;
+        private readonly IMapper _mapper;
 
         public SalesController(
-            IMediator mediator)
+            IMediator mediator,
+            ISaleRepository repo,
+            IMapper mapper)
         {
             _mediator = mediator;
+            _repo = repo;
+            _mapper = mapper;
         }
 
         [HttpPost]
         public async Task<IActionResult> Create(SaleDto dto)
         => Ok(await _mediator.Send(new CreateSaleCommand(dto)));
 
-        
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+            => Ok((await _repo.GetAllAsync()).Select(_mapper.Map<SaleDto>));
+
     }
 }
